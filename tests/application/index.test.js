c64c1a11a73866fa672d0f2fb2eda0836f014910
Garbin Huang <garbinh@gmail.com>
2019-01-27 11:23:42 +0800

    add application base & tests

---
+++ tests/application/index.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const { Application } = require('../../lib')
+const { describe, expect, test, afterEach } = global
+describe('application', () => {
+  let server
+  afterEach(() => {
+    server && server.close()
+  })
+  test('should have koa-qs wrappered', async () => {
+    const app = new Application()
+    app.use(async ctx => {
+      expect(ctx.request.query.a).toEqual(['1', '2', '3'])
+      expect(ctx.request.querystring).toBe('a[0]=1&a[1]=2&a[2]=3')
+      ctx.status = 204
+    })
+    server = app.listen()
+    const response = await request(server).get('/?a[0]=1&a[1]=2&a[2]=3')
+    expect(response.status).toBe(204)
+  })
+  // test('restful presets should work', async () => {
+  //   const app = new Application()
+  //   server = app.listen()
+  //   const response = await request(server).get('/')
+  //   expect(response.status).toBe(200)
+  // })
+})
