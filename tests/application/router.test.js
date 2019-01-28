7c6dc2ff63617d6c48b098d05ec11d93347d5bb7
Garbin Huang <garbinh@gmail.com>
2019-01-28 13:11:28 +0800

    add tester.read & tester.create

--- tests/application/router.test.js
+++ tests/application/router.test.js
@@ -3,30 +3,23 @@ const { server, routers, knex } = require('../_lib/app')
 const { test, afterAll, expect } = global
 
 afterAll(() => { knex.destroy(); server.close() })
-test.restful(server, routers.posts, tester => {
+test.restful(server, routers.posts, ({ prepare, create, read }) => {
   let item
-  tester.prepare({
+  prepare({
     title: casual.title,
     contents: casual.text,
     slug: casual.word
   }, ctx => { item = ctx })
-  // tester.prepareEach(ctx => ({
-  //   title: casual.title,
-  //   contents: casual.text,
-  //   slug: casual.word
-  // }), ctx => {
-  //   item = ctx
-  // })
-  // test('item', () => {
-  //   expect(item).toBeInstanceOf(models.Post)
-  // })
-  tester.create(ctx => ({
+  create(ctx => ({
     title: casual.title,
     contents: casual.text,
     slug: `${casual.word}-${Math.random() * 10000}`
   })).test()
-// tester.read.list().test()
-// tester.read.item(item).test()
+  read.list().test()
+  test('item', () => {
+    expect(item).toBeInstanceOf(routers.posts.model)
+  })
+  read.item(() => item.id).test()
 // tester.update(item, { title: 'updated' }).assert(res => {
 //   expect(res.status).toBe(202)
 //   expect(res.body.title).toBe('updated')
